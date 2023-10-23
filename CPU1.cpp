9fbc93fe78ab28dc9954ebea9697f504a687906d
RafaelDiaz22 <148782769+RafaelDiaz22@users.noreply.github.com>
2023-10-23 08:42:39 -0600

    Add files via upload

---
+++ CPU1.cpp
@@ -0,0 +1,21 @@
+#include<ctime> //biblioteca de funciones de reloj
+#include<iostream>
+#include<math.h>
+using namespace std;
+void mostrarTiempo(clock_t tiempo);
+int main(){
+	clock_t tiempo = clock();
+	long long i = 0;
+	double sum = 0;
+	for (i=1;i<1000000000;i++){
+		sum = sum +log(double(i));
+	}
+	mostrarTiempo(tiempo);
+	cout <<"Pulse para salir....";
+	cin.get();
+	return 0;
+}
+void mostrarTiempo(clock_t tiempo){
+	tiempo = clock()- tiempo;
+	cout <<endl << "Tiempo de ejecucion es "<<double(tiempo)/ CLOCKS_PER_SEC << " seg. "<<endl;
+}
